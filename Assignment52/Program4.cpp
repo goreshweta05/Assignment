623e3f6d6e02763d10504f571353f2729b7412f9
Shweta Gore <goreshweta05@gmail.com>
2026-01-19 12:16:50 +0530

    Add files via upload

---
+++ Assignment52/Program4.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+template <class T>
+
+T Max(T *arr,int iSize)
+{
+   T iMax = arr[0];
+   int i = 0;
+
+   for(i = 1; i < iSize;i++)
+   {
+      if(arr[i] > iMax)
+     {
+       iMax = arr[i];
+     }
+   }
+   return iMax;
+}
+int main()
+{
+    int Arr[] = {10,20,30,40,50};
+    float Brr[] = {10.0,3.7,9.8,8.7};
+
+    int iRet = Max(Arr,5);
+    printf("%d\n",iRet);
+
+    float fRet = Max(Brr,4);
+    printf("%f\n",fRet);
+
+    return 0;
+}
---
+++ Assignment52/Program5.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+template <class T>
+
+T Min(T *arr,int iSize)
+{
+   T iMin = arr[0];
+   int i = 0;
+
+   for(i = 0;i < iSize;i++)
+   {
+    if(arr[i] < iMin)
+    {
+      iMin = arr[i];
+    }
+   }
+   return iMin;
+}
+int main()
+{
+    int Arr[] = {10,20,30,40,50};
+    float Brr[] = {10.0,3.7,9.8,8.7};
+
+    int iRet = Min(Arr,5);
+    printf("%d\n",iRet);
+
+    float fRet = Min(Brr,4);
+    printf("%f\n",fRet);
+
+    return 0;
+}
